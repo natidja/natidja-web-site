a48392b381add53c62b3ea926ac265d2d24e644f
baba bendermel houssem edidne <houssemeddine.bababendermel@univ-tlemcen.dz>
2021-08-08 23:23:06 +0100

    modif sur la bdd

--- bdd/natija.sql
+++ bdd/natija.sql
@@ -1,126 +1,88 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Ven 06 Août 2021 à 22:53
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Host: 127.0.0.1
+-- Generation Time: Aug 09, 2021 at 12:22 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.3.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `natija`
+-- Database: `natija`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `labo`
+-- Table structure for table `patient`
 --
 
-CREATE TABLE IF NOT EXISTS `labo` (
-  `id_labo` int(10) NOT NULL AUTO_INCREMENT,
-  `nom_labo` varchar(30) NOT NULL,
-  `wilaya` varchar(20) NOT NULL,
-  `adresse` varchar(30) NOT NULL,
-  `numero` int(10) NOT NULL,
-  `adresse_email` varchar(30) NOT NULL,
-  `pwd` varchar(10) NOT NULL,
-  PRIMARY KEY (`id_labo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
---
--- Contenu de la table `labo`
---
-
-INSERT INTO `labo` (`id_labo`, `nom_labo`, `wilaya`, `adresse`, `numero`, `adresse_email`, `pwd`) VALUES
-(3, 'natidja', 'Tlemcen', 'Imama', 550505050, 'natidja@gmail.com', 'acidome'),
-(4, 'Likan', 'Oran', 'IDK', 560606060, 'likan@gmail.com', 'tweel');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `patient`
---
-
-CREATE TABLE IF NOT EXISTS `patient` (
-  `id_p` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `patient` (
+  `id_p` int(10) NOT NULL,
   `pseudo` varchar(30) DEFAULT NULL,
   `mdp` varchar(20) DEFAULT NULL,
   `nom_p` varchar(20) NOT NULL,
   `prenom_p` varchar(20) NOT NULL,
   `date_naissance` date NOT NULL,
   `id_test` int(10) NOT NULL,
-  `nbr_test` int(1) NOT NULL,
-  PRIMARY KEY (`id_p`),
-  KEY `fk_resultat` (`id_test`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=486 ;
+  `nbr_test` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `patient`
+-- Dumping data for table `patient`
 --
 
 INSERT INTO `patient` (`id_p`, `pseudo`, `mdp`, `nom_p`, `prenom_p`, `date_naissance`, `id_test`, `nbr_test`) VALUES
-(474, 'bekhtaoui_radia', 'qYNVt4Oy', 'bekhtaoui', 'radia', '2021-08-18', 327, 10),
+(474, 'bekhtaoui_radia', 'qYNVt4Oy', 'bekhtaoui', 'radia', '2021-08-18', 327, 2),
 (475, 'ziani_mohammed', 'wBuNm9Rt', 'ziani', 'mohammed', '2021-08-13', 328, 1),
-(485, 'ziani_issam', 'lt4O8NVs', 'ziani', 'issam', '2021-08-20', 347, 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `test`
---
-
-CREATE TABLE IF NOT EXISTS `test` (
-  `id_test` int(10) NOT NULL AUTO_INCREMENT,
-  `nom_p` varchar(20) NOT NULL,
-  `prenom_p` varchar(20) NOT NULL,
-  `date_naissance` date NOT NULL,
-  `resultat` varchar(10) DEFAULT NULL,
-  `sexe` varchar(6) NOT NULL,
-  `avatar` varchar(255) DEFAULT NULL,
-  `date_test` datetime DEFAULT NULL,
-  `id_labo` int(10) NOT NULL,
-  PRIMARY KEY (`id_test`),
-  KEY `fk_id_labo` (`id_labo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=348 ;
+(485, 'ziani_issam', 'lt4O8NVs', 'ziani', 'issam', '2021-08-20', 347, 1),
+(487, 'malti_djellel', 'nKq5vO1s', 'malti', 'djellel', '0001-01-01', 349, 1),
+(488, 'bekhtaoui_radia', 'Fo55ctAa', 'bekhtaoui', 'radia', '2021-08-18', 350, 1);
 
 --
--- Contenu de la table `test`
---
-
-INSERT INTO `test` (`id_test`, `nom_p`, `prenom_p`, `date_naissance`, `resultat`, `sexe`, `avatar`, `date_test`, `id_labo`) VALUES
-(327, 'bekhtaoui', 'radia', '2021-08-18', 'prÃ©s', 'femme', 'test1.pdf', '2021-08-06 06:32:32', 3),
-(328, 'ziani', 'mohammed', '2021-08-13', 'prÃ©s', 'homme', 'test2.pdf', '2021-08-06 06:36:26', 3),
-(343, 'bekhtaoui', 'radia', '2021-08-18', 'prÃ©s', 'femme', '', '2021-08-06 08:52:02', 3),
-(347, 'ziani', 'issam', '2021-08-20', 'en attente', 'homme', '', '2021-08-06 08:55:10', 3);
-
---
--- Contraintes pour les tables exportées
+-- Indexes for dumped tables
 --
 
 --
--- Contraintes pour la table `patient`
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`id_p`),
+  ADD KEY `fk_resultat` (`id_test`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `id_p` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=489;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `patient`
 --
 ALTER TABLE `patient`
   ADD CONSTRAINT `fk_date_naissance` FOREIGN KEY (`id_test`) REFERENCES `test` (`id_test`),
   ADD CONSTRAINT `fk_id_test` FOREIGN KEY (`id_test`) REFERENCES `test` (`id_test`),
   ADD CONSTRAINT `fk_nom_p` FOREIGN KEY (`id_test`) REFERENCES `test` (`id_test`),
   ADD CONSTRAINT `fk_prenom_p` FOREIGN KEY (`id_test`) REFERENCES `test` (`id_test`);
-
---
--- Contraintes pour la table `test`
---
-ALTER TABLE `test`
-  ADD CONSTRAINT `fk_id_labo` FOREIGN KEY (`id_labo`) REFERENCES `labo` (`id_labo`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
