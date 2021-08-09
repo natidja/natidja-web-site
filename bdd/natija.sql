25a7c0c54e3f3daf3d265f2e845992972c1919ea
baba bendermel houssem edidne <houssemeddine.bababendermel@univ-tlemcen.dz>
2021-08-09 22:16:02 +0100

    modif sur bdd ._.

--- bdd/natija.sql
+++ bdd/natija.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 09, 2021 at 12:22 AM
+-- Generation Time: Aug 09, 2021 at 11:15 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.3.26
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `natija`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `labo`
+--
+
+CREATE TABLE `labo` (
+  `id_labo` int(10) NOT NULL,
+  `nom_labo` varchar(30) NOT NULL,
+  `wilaya` varchar(20) NOT NULL,
+  `adresse` varchar(30) NOT NULL,
+  `numero` int(10) NOT NULL,
+  `adresse_email` varchar(30) NOT NULL,
+  `pwd` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `labo`
+--
+
+INSERT INTO `labo` (`id_labo`, `nom_labo`, `wilaya`, `adresse`, `numero`, `adresse_email`, `pwd`) VALUES
+(3, 'natidja', 'Tlemcen', 'Imama', 550505050, 'natidja@gmail.com', 'acidome'),
+(4, 'Likan', 'Oran', 'IDK', 560606060, 'likan@gmail.com', 'tweel');
 
 -- --------------------------------------------------------
 
@@ -49,9 +73,45 @@ INSERT INTO `patient` (`id_p`, `pseudo`, `mdp`, `nom_p`, `prenom_p`, `date_naiss
 (487, 'malti_djellel', 'nKq5vO1s', 'malti', 'djellel', '0001-01-01', 349, 1),
 (488, 'bekhtaoui_radia', 'Fo55ctAa', 'bekhtaoui', 'radia', '2021-08-18', 350, 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `id_test` int(10) NOT NULL,
+  `nom_p` varchar(20) NOT NULL,
+  `prenom_p` varchar(20) NOT NULL,
+  `date_naissance` date NOT NULL,
+  `resultat` varchar(10) DEFAULT NULL,
+  `sexe` varchar(6) NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `date_test` date DEFAULT NULL,
+  `id_labo` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id_test`, `nom_p`, `prenom_p`, `date_naissance`, `resultat`, `sexe`, `avatar`, `date_test`, `id_labo`) VALUES
+(327, 'bekhtaoui', 'radia', '2021-08-18', 'pres', 'femme', 'test1.pdf', '2021-08-06', 3),
+(328, 'ziani', 'mohammed', '2021-08-13', 'pres', 'homme', 'test2.pdf', '2021-08-06', 3),
+(343, 'bekhtaoui', 'radia', '2021-08-18', 'pres', 'femme', '', '2021-08-06', 3),
+(347, 'ziani', 'issam', '2021-08-20', 'en attente', 'homme', '', '2021-08-06', 3),
+(349, 'malti', 'djellel', '0001-01-01', 'en attente', 'femme', '', '2021-08-07', 3),
+(350, 'bekhtaoui', 'radia', '2021-08-18', 'en attente', 'femme', NULL, '2021-08-08', 4);
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `labo`
+--
+ALTER TABLE `labo`
+  ADD PRIMARY KEY (`id_labo`);
 
 --
 -- Indexes for table `patient`
@@ -61,14 +121,33 @@ ALTER TABLE `patient`
   ADD KEY `fk_resultat` (`id_test`);
 
 --
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id_test`),
+  ADD KEY `fk_id_labo` (`id_labo`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `labo`
+--
+ALTER TABLE `labo`
+  MODIFY `id_labo` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
   MODIFY `id_p` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=489;
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id_test` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=351;
 
 --
 -- Constraints for dumped tables
@@ -82,6 +161,12 @@ ALTER TABLE `patient`
   ADD CONSTRAINT `fk_id_test` FOREIGN KEY (`id_test`) REFERENCES `test` (`id_test`),
   ADD CONSTRAINT `fk_nom_p` FOREIGN KEY (`id_test`) REFERENCES `test` (`id_test`),
   ADD CONSTRAINT `fk_prenom_p` FOREIGN KEY (`id_test`) REFERENCES `test` (`id_test`);
+
+--
+-- Constraints for table `test`
+--
+ALTER TABLE `test`
+  ADD CONSTRAINT `fk_id_labo` FOREIGN KEY (`id_labo`) REFERENCES `labo` (`id_labo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
