4d2b036aea054e29f3a295011e9d865026a97453
baba bendermel houssem edidne <houssemeddine.bababendermel@univ-tlemcen.dz>
2021-08-13 23:51:55 +0100

    supprimer fix

--- supprimer.php
+++ supprimer.php
@@ -1,16 +1,40 @@
 <?php
-include("Auth_pub.php"); 
+include("Auth_pub.php");
 
 
     $id_test = $_POST['delete_nomp'];
-    $requete_sql = "DELETE FROM patient where id_test = '$id_test'";
 
-        $result = $conn->query($requete_sql);
+    $requete_m= "select * from test where id_test='$id_test' ";
+    $result_m = $conn->query($requete_m);
 
-    $requete_sql2 = "DELETE FROM test where id_test = '$id_test'";
+    while($tuple = $result_m->fetch(PDO::FETCH_ASSOC)){//Retourner des tableaux associatifs
+        $nom_p = $tuple['nom_p'];
+        $prenom_p = $tuple['prenom_p'];
+        $date_naissance = $tuple['date_naissance'];
+    }
 
-    $result2 = $conn->query($requete_sql2);
-   
+    //suppression des deux tables
+            $requete_sql0 = "SELECT * FROM patient where nom_p='$nom_p'and prenom_p='$prenom_p'and date_naissance='$date_naissance'";
+            $result0 = $conn->query($requete_sql0);
+
+            while($tuple0 = $result0->fetch(PDO::FETCH_ASSOC)){//Retourner des tableaux associatifs
+                $id_p = $tuple0['id_p'];
+                $nbr_test = $tuple0['nbr_test'];
+            }
+            if($nbr_test == 1){
+                $requete_sql = "DELETE FROM patient where id_p = '$id_p'";
+                $conn->query($requete_sql);
+
+                $requete_sql2 = "DELETE FROM test where id_test = '$id_test'";
+                $conn->query($requete_sql2);
+            }
+            else{
+                $requete_sql3 = "UPDATE patient SET nbr_test= nbr_test-1 where id_p= '$id_p'";
+                $conn->exec($requete_sql3);
+
+                $requete_sql4 = "DELETE FROM test where id_test = '$id_test'";
+                $conn->query($requete_sql4);
+            }
 
     header("Location: labo_home.php");
 ?>
